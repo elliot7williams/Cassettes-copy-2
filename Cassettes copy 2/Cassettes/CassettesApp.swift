fed4178c92b50d54fc8429fe4eda2bf5089bb1b1
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:30:45 -0400

    Add Cassettes copy 2/Cassettes/CassettesApp.swift

---
+++ Cassettes copy 2/Cassettes/CassettesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CassettesApp.swift
+//  Cassettes
+//
+//  Created by Elliot Williams on 2025-05-31.
+//
+
+import SwiftUI
+
+@main
+struct CassettesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CassettePlayerView()
+        }
+    }
+}
