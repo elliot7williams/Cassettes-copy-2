8504a831ab379334b6ecfe53edf93834c2918512
elliot7williams <58864955+elliot7williams@users.noreply.github.com>
2026-08-01 22:30:55 -0400

    Add Cassettes copy 2/CassettesTests/CassettesTests.swift

---
+++ Cassettes copy 2/CassettesTests/CassettesTests.swift
@@ -0,0 +1,17 @@
+//
+//  CassettesTests.swift
+//  CassettesTests
+//
+//  Created by Elliot Williams on 2025-05-31.
+//
+
+import Testing
+@testable import Cassettes
+
+struct CassettesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
